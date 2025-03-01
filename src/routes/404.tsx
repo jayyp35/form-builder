cc113d94cd85eeb502f50fe516292f8e0db17d68
jayyp35-2 <j7prabhu@gmail.com>
2025-03-01 11:36:09 +0530

    routes setup

---
+++ src/routes/404.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+// import Button from "../common/Button/Button";
+import { useLocation, useNavigate } from "react-router-dom";
+
+export default function InvalidRoute() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") navigate("/");
+  }, [location.pathname]);
+
+  return (
+    <div className={"NotFound"}>
+      <div className={"Box"}>
+        Invalid Route
+        {/* <Button
+          onClick={() => {
+            navigate(-1);
+          }}
+          text={"Go Back"}
+        /> */}
+      </div>
+    </div>
+  );
+}
