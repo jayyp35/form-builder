bcddcd8ac5b8dac4791b4224696a4c2b3e0e5b56
jayyp35-2 <j7prabhu@gmail.com>
2025-03-02 15:54:48 +0530

    --

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/form-builder/", // Replace 'repository-name' with your repository name
   plugins: [react()],
-})
+});
