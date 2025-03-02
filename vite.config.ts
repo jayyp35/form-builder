6d6d2bd02b7377ec7dc63f19aa0dae3916f7e90f
jayyp35-2 <j7prabhu@gmail.com>
2025-03-02 15:58:38 +0530

    --

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/form-builder/", // Replace 'repository-name' with your repository name
+  base: "/", // Replace 'repository-name' with your repository name
   plugins: [react()],
 });
